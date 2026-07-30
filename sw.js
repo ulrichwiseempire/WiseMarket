81a0012ce84e6fdd5fb604f2cbe33a12ec361ae5
ulrichwiseempire <ulrichwiseempire1@gmail.com>
2026-07-30 06:55:05 +0000

    sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
